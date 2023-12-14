de1c2974e999a8d30bb8970c1aa72689aff11ca0
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-14 23:30:31 +0500

    simple

---
+++ js/for.js
@@ -0,0 +1,232 @@
+// for  1
+
+// let a = 10;
+// let b = 20;
+// count = 0;
+
+
+// for(let i = a; i <= b; i++){
+//     console.log(i);
+//     count ++ ;
+// }
+// console.log("count =",count);
+
+// for 2
+
+// let a = 10;
+// let b = 20;
+// count = 0;
+
+
+// for(let i = (b - 1); i > a; i--){
+//     console.log(i);
+//     count ++ ;
+// }
+// console.log("count =",count);
+
+// for3 
+
+// let kg = 2000;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i*kg);
+// }
+
+// for 4
+
+
+// let kg = 2000;
+
+// for(let i = 12; i <= 20; i++){
+//     console.log((kg * i)/10);
+// }
+
+// for 5
+
+// let a =1;
+// let b =10;
+// count = 0;
+
+// for(let i = a; i <= b; i++){
+//     count +=i;
+// }
+// console.log(count);
+
+// for 6
+
+// let a = 1;
+// let b = 5;
+// let mull = 1
+
+// for(let i = a; i <= b ; i++){
+//     mull *=i;
+// }
+// console.log(mull);
+
+// for 7
+
+// let a = 1;
+// let b = 5;
+// let pull = 0;
+
+// for(let i = a;i <= b; i++){
+//     pull += i ** 2;
+// }
+// console.log(pull);
+
+// for 8
+
+// let n = 5;
+// let pull = 0;
+
+// for(let i = 1; i <= n; i++){
+//     pull += 1 / i;
+// }
+// console.log(pull);
+
+// for 9
+
+// let n = 20;
+// let mull = 1;
+
+// for(let i = 11; i <= n; i++){
+//     mull *=(i / 10);
+// }
+// console.log(mull);
+
+// for 10
+
+// let n =5;
+
+// let pow = 0;
+
+// for(let i = 1; i <= (2*n -1); i +=2){
+//    pow +=i;
+// }
+// console.log(pow);
+
+// for11
+
+// for 12
+
+// let a = 2;
+// let n = 10 ;
+
+// pow = 1;
+
+// for(let i = 1; i <= n; i++){
+//      pow = a ** i;
+//      console.log(pow);
+//     }
+
+// for 13
+
+// let a = 1.5;
+// let n = 10 ;
+
+// pow = 0;
+// pull = 0;
+
+// for(let i = 1; i <= n; i++){
+//      pow = (a ** i);
+//      pull +=  (a ** i);
+//      console.log(pow);
+//     }
+//     console.log("pull =", pull +1 );
+
+// for 14
+
+// let n = 5;
+// pull = 0;
+// jull = 0
+
+// for(let i = 1; i <= n; i++){
+//     for(let j = 1; j <= i; j++){
+//          jull += j;
+//          pull += i + jull
+//          console.log(j)
+//         }
+//     }
+//     console.log(jull)  Qayta ko'rishim kerak!
+
+// for 15
+
+// let n = 5;
+// let k = 2
+// pull = 0;
+
+// for(let i = 1; i <= n; i++){
+//     pull += i ** k;
+// }
+// console.log(pull);
+
+// for 16
+
+// let n = 5;
+// pull = 0;
+
+// for(let i = 1; i <= n; i++){
+//     pull += i ** 2;
+// }
+// console.log(pull);
+
+// for 17
+
+// let A = 2;
+// let B = 5;
+// pull = 0;
+
+// for(let i = A; i <= B; i++){
+//     for(let j = 1; j <= i; j++){
+//         console.log(i);
+//     }
+// }
+
+// for 18
+
+// let n = 20;
+// pull = 0;
+// count = 0;
+
+// for(let i = 1; i <= n / 2; i++){
+//     if(n % i == 0){     
+//         pull += i;
+//     }
+//     count ++;
+// }
+// console.log(pull);
+// console.log(count);
+
+// for 19
+
+// let n = 11;
+
+// for(let i = 1; i <= n / 2; i++){
+//     if(n % i ===0){
+//         console.log(n,"murakkab son");
+//     }else if(n % 2 === 1){
+//         console.log(n, "tub son")
+//     }
+// }
+
+// for 20
+
+// let a = 1;
+// let n = 5;
+
+// for(let i = a;i <= n; i++){
+//     for(let j = 1; j <= i; j++){
+//         console.log(j);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
